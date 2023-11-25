27d1f65a1e3b8666468377b302b334330c9ffaeb
ColdWindScholar <3590361911@qq.com>
2023-11-25 18:48:00 +0800

    init

--- Flash.py
+++ Flash.py
@@ -1,8 +1,58 @@
-import tkinter as tk
+import os
+import subprocess
+from os import getcwd, sep
+from threading import Thread
+
 import sv_ttk
 from tkinter import *
 from tkinter.ttk import *
-import tkinter.ttk as ttk
+
+osname = os.name
+
+
+def cz(func, *args):
+    Thread(target=func, args=args, daemon=True).start()
+
+
+def call(exe, kz='Y', out=0, shstate=False, sp=0):
+    if kz == "Y":
+        cmd = f'{getcwd()}{sep}{exe}'
+    else:
+        cmd = exe
+    if osname != 'posix':
+        conf = subprocess.CREATE_NO_WINDOW
+    else:
+        if sp == 0:
+            cmd = cmd.split()
+        conf = 0
+    try:
+        ret = subprocess.Popen(cmd, shell=shstate, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, creationflags=conf)
+        for i in iter(ret.stdout.readline, b""):
+            if out == 0:
+                print(i.decode("utf-8", "ignore").strip())
+    except subprocess.CalledProcessError as e:
+        for i in iter(e.stdout.readline, b""):
+            if out == 0:
+                print(e.decode("utf-8", "ignore").strip())
+    ret.wait()
+    return ret.returncode
+
+
+class StdoutRedirector(object):
+    def __init__(self, text_widget):
+        self.text_space = text_widget
+
+    def write(self, string):
+        self.text_space.insert(END, string)
+        self.text_space.yview('end')
+
+    @staticmethod
+    def flush():
+        pass
+
+    def __exit__(self):
+        pass
 
 
 class Center_Show(object):
