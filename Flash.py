cfc394e3c18976c80ab7f45dac77bb1e1fd6aec3
ColdWindScholar <3590361911@qq.com>
2023-11-25 18:44:34 +0800

    init

--- Flash.py
+++ Flash.py
@@ -5,11 +5,27 @@ from tkinter.ttk import *
 import tkinter.ttk as ttk
 
 
+class Center_Show(object):
+    def __init__(self, master):
+        self.master = master
+        self.set()
+
+    def set(self):
+        self.master.geometry('+{}+{}'.
+                             format(int(self.master.winfo_screenwidth() / 2 - self.master.winfo_width() / 2),
+                                    int(self.master.winfo_screenheight() / 2 - self.master.winfo_height() / 2)))
+
+
 class FlashTool(Tk):
     def __init__(self):
         super().__init__()
         self.title('MIO-KITCHEN-FLASH-TOOL')
         sv_ttk.use_dark_theme()
+        self.controls()
+        Center_Show(self)
+
+    def controls(self):
+        Label(self, text="MIO-KITCHEN-FLASH-TOOL", font=(None, 20)).pack()
 
 
 if __name__ == '__main__':
